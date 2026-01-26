e59fa95b86e79ddcd9ac6794f17eb46a5ab8968d
Sergio Sánchez Martín <ssanmar580q@g.educaand.es>
2026-01-26 14:34:02 +0100

    Clase 26/01/2026

--- Unidad 03/dom-intro/app.js
+++ Unidad 03/dom-intro/app.js
@@ -13,3 +13,23 @@ app.innerHTML = `
             <strong>Menos mal que empieza el finde.</strong>
         </p>
     </section>`;
+
+// Métodos modernos para modificar contenido
+// console.log("Método moderno GET HTML: " + app.getHTML());
+const cod = `<p><strong>setHTMLUnsafe</strong> es la alternativa moderna 
+a innerHTML para modificar contenido HTML</p>`;
+app.setHTMLUnsafe(cod);
+
+// Crear elementos HTML
+// Método createElement("etiqueta")
+const seccion = document.createElement("section");
+console.log("El elemento seccion aun no está conectado o insertado:", seccion.isConnected);
+seccion.innerHTML = `<p>Estoy inyectando código HTML a la sección</p>`;
+
+// Añado la nueva sección al div cuyo id es app
+app.appendChild(seccion);
+console.log(seccion.isConnected);       //true porque ya está insertada
+
+// Si quisiera clonar el elemento seccion
+const copiaSeccion = seccion.cloneNode();
+const copiaProfunda = seccion.cloneNode(true);
